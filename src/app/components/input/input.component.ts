d4835da73d485610c4efbeeca3cf2a75d5a948ec
Joan-Maina <mainajoan12@gmail.com>
2022-03-07 14:41:17 +0300

    add navbar

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -8,20 +8,41 @@ import { Component, Input, OnInit } from '@angular/core';
 export class InputComponent implements OnInit {
 
   constructor() { }
+
+  constant: number = 1;
+
   money: any = {
     amount: 0
   };
+
+  selectedCurrency: string = '';
+
   ngOnInit(): void {
   }
-  // foods = [
-  //   { value: 'kenyan shillings' },
-  //   { value: 'dollars' },
-  //   // { value: 'euros' },
-  //   // { value: 'pounds' },
-  //   // { value: 'japanese yen' },
-  //   // { value: 'jamaican dollar' }
-  // ]
+  foods = [
+    { value: 'kenyan shillings' },
+    { value: 'dollars' },
+    { value: 'euros' },
+    { value: 'pounds' },
+    { value: 'japanese yen' },
+    { value: 'jamaican dollar' }
+  ]
 
-
+  onChange(data: string) {
+    if (data == 'kenyan shillings') {
+      this.constant = 1;
+    } else if (data == 'euros') {
+      this.constant = 0.0081;
+    } else if (data == 'pounds') {
+      this.constant = 0.0067;
+    } else if (data == 'japanese yen') {
+      this.constant = 1.01;
+    } else if (data == 'jamaican dollar') {
+      this.constant = 1.37;
+    }
+    else {
+      this.constant = 0.009
+    }
+  }
 
 }
